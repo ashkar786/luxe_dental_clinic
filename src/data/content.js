9daee9a52bc331a79e8ec79db177d3cdb131fea4
ashkar786 <68378136+ashkar786@users.noreply.github.com>
2026-07-31 23:35:03 +0530

    Update WhatsApp URL for contact information

--- src/data/content.js
+++ src/data/content.js
@@ -5,7 +5,7 @@ import drMostafaKhalaf from '../images/Dr_Mostafa_Khalaf.png'
 import drSeharBanuSadatulla from '../images/Dr_Sehar_Banu_Sadatulla.png'
 import drMohamedAbosekina from '../images/Dr_Mohamed_Abosekina.png'
 
-export const WHATSAPP_URL = 'https://wa.me/97143580885'
+export const WHATSAPP_URL = 'https://wa.me/+971543580995'
 export const PHONE_TEL = 'tel:+971 54 358 0995'
 export const PHONE_DISPLAY = '+971 54 358 0995'
 
